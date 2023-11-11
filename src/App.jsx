f8e893e7eb8ae7e381da26454934a864b142766f
Roland-Sankara <roland1sankara@gmail.com>
2023-11-11 13:32:03 +0300

    use the link element

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,38 @@
 import Home from "./pages/Home";
 import Quotes from "./pages/Quotes";
 import Faqs from "./pages/Faqs";
-import {BrowserRouter, Routes, Route} from "react-router-dom"
+import {BrowserRouter, Routes, Route, Link} from "react-router-dom"
 import "./App.css";
 
 function App(){
   return(
-    <div>
-      {/* Navigation Menu */}
-      <header>
-        <nav>
-          <ul>
-            <li>
-              <a href="/">Home</a>
-            </li>
-            <li>
-              <a href="/quotes">Quotes</a>
-            </li>
-            <li>
-              <a href="/faqs">FAQs</a>
-            </li>
-          </ul>
-        </nav>
-      </header>
-
-      <BrowserRouter>
-
-        <Routes>
-          <Route path="/" element={<Home/>}/>
-          <Route path="/quotes" element={<Quotes/>}/>
-          <Route path="/faqs" element={<Faqs/>}/>
-        </Routes>
-
-      </BrowserRouter>
+    <BrowserRouter>
+      <div>
+        {/* Navigation Menu */}
+        <header>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/quotes">Quotes</Link>
+              </li>
+              <li>
+                <Link to="/faqs">FAQs</Link>
+              </li>
+            </ul>
+          </nav>
+        </header>
 
 
-    </div>
+          <Routes>
+            <Route path="/" element={<Home/>}/>
+            <Route path="/quotes" element={<Quotes/>}/>
+            <Route path="/faqs" element={<Faqs/>}/>
+          </Routes>
+      </div>
+    </BrowserRouter>
   )
 }
 
